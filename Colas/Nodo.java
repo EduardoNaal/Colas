6685f41e475866c42fe3bfee0e37808a3fd5588a
Eduardo Javier Naal <131737602+EduardoNaal@users.noreply.github.com>
2023-09-19 00:13:35 -0600

    Add files via upload

--- Colas/Nodo.java
+++ Colas/Nodo.java
@@ -1,20 +1,27 @@
+
 public class Nodo {
-private int dato;
-private Nodo siguiente;
-public Nodo(int dato) {
-this.dato = dato;
-this.siguiente = null;
-}
-public int getDato() {
-return dato;
-}
-public void setDato(int dato) {
-this.dato = dato;
-}
-public Nodo getSiguiente() {
-return siguiente;
-}
-public void setSiguiente(Nodo siguiente) {
-this.siguiente = siguiente;
-}
+
+    private int dato;
+    private Nodo siguiente;
+
+    public Nodo(int dato) {
+        this.dato = dato;
+        this.siguiente = null;
+    }
+
+    public int getDato() {
+        return dato;
+    }
+
+    public void setDato(int dato) {
+        this.dato = dato;
+    }
+
+    public Nodo getSiguiente() {
+        return siguiente;
+    }
+
+    public void setSiguiente(Nodo siguiente) {
+        this.siguiente = siguiente;
+    }
 }
